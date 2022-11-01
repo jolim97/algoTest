214f6fdcc1774585d176d7b2818d5560933108dc
jolim97 <physicsjong1@gmail.com>
2022-11-01 23:41:02 +0900

    18108 java

---
+++ boj/java/18108.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] arg) {
+        Scanner sc = new Scanner(System.in);
+        int y = sc.nextInt();
+        System.out.println(y-(2541-1998));
+    }
+}
