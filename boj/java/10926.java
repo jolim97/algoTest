11a0bf4998849df3967545ce9326d149712d4cf3
jolim97 <physicsjong1@gmail.com>
2022-11-01 23:49:52 +0900

    10926 java

---
+++ boj/java/10926.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String[] arg) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(str+"??!");
+    }
+}
