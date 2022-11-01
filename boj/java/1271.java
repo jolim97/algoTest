00486de044817b78a9511b0abef71ee51c9f2e5f
jolim97 <physicsjong1@gmail.com>
2022-11-02 00:25:53 +0900

    1271 java

---
+++ boj/java/1271.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.math.BigInteger;
+
+class Main {
+    public static void main(String[] arg){
+        Scanner sc = new Scanner(System.in);
+        BigInteger n = sc.nextBigInteger();
+        BigInteger m = sc.nextBigInteger();
+        System.out.println(n.divide(m));
+        System.out.println(n.remainder(m));
+    }
+}
